4d0c80cbaca5844b34fb384f2e4e7d8a8e383e3f
Tatakai <3021536220@qq.com>
2025-12-31 14:27:09 +0800

    init: backend

---
+++ EXAM-SYSTEM-BACKEND/src/main/java/com/csl/examsystembackend/ExamSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.csl.examsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamSystemBackendApplication.class, args);
+    }
+
+}
---
+++ EXAM-SYSTEM-BACKEND/src/test/java/com/csl/examsystembackend/ExamSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csl.examsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
